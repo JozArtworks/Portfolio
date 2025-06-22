f0370f7c1fb703e46a476049d7fee3782c8db8cd
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-06-23 00:13:12 +0200

    🎨✨ feat & style(about me): animation fade in and out blockqoute & style responsive optimize

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
+
   slogans = [
     '"Ordnung im Code ist kein Luxus. Es ist Verantwortung."',
     '"Code ist Kommunikation."',
@@ -16,37 +17,25 @@ export class AboutMeComponent implements OnInit {
     '"Frontend ist kein Klickziel – sondern Systemschnittstelle."',
     '"Technik ist kein Stilmittel. Sie ist Verantwortung."',
     '"Der Nutzer sieht’s nicht. Aber der nächste Entwickler schon."',
-    '"Mein Handwerk beginnt dort, wo Funktion auf Bedeutung trifft."',
-    '"Design ist für mich kein Stil. Es ist Haltung. Genau wie sauberer Code."',
     '"Mein Stack? HTML, CSS, TypeScript – und eine Prise Intuition."',
-    '"Erlebnisse entstehen, wenn Gestaltung führt und Technik trägt."',
-    '"Ein gutes Frontend beginnt nicht beim Layout, sondern bei der Entscheidung, was wirklich gebraucht wird."',
-    '"Sauberer Code ist kein Ziel – er ist die Voraussetzung für alles, was danach kommt."',
-    '"Verantwortung im Code zeigt sich nicht in Features – sondern in den Fehlern, die vermieden wurden."',
-    '"Gutes Frontend ist, wenn sich niemand fragt, wie es funktioniert – weil es einfach funktioniert."',
-    '"Daten fließen, Layouts folgen – der Rest ist Fokus."',
-    '"Frontend ist Systemdenken – und jede Zeile Code formt Vertrauen."',
-    '"Was verständlich ist im Code, bleibt erfassbar im Interface."',
-    '"Gute Architektur führt nicht nur den Nutzer – sondern auch den Entwickler von morgen."',
-    '"Ich glaube an Code, der nicht nur funktioniert – sondern auch lesbar bleibt."'
+    '"Daten fließen, Layouts folgen – der Rest ist Fokus."'
   ];
 
   currentSlogan = this.slogans[0];
   sloganIndex = 0;
-  animationState: 'visible' | 'hidden' = 'visible';
+  animationState: 'fade-in' | 'fade-out' = 'fade-in';
+
 
   ngOnInit(): void {
     setInterval(() => {
-      this.animationState = 'hidden';
-
-
+      this.animationState = 'fade-out';
       setTimeout(() => {
         this.sloganIndex = (this.sloganIndex + 1) % this.slogans.length;
         this.currentSlogan = this.slogans[this.sloganIndex];
-        this.animationState = 'visible';
+        this.animationState = 'fade-in';
 
-        
       }, 400);
     }, 7000);
   }
+
 }
