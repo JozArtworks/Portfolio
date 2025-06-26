6c6d62152b9b91f38c62b496adf7493b480f3648
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-06-26 17:39:27 +0200

    ✨ feat(skills): add skills html, scss & ts start with structure and responsive

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -9,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  toolIcons = [
+  { name: 'Angular', src: 'assets/icons/white/angular_white.png' },
+  { name: 'TypeScript', src: 'assets/icons/white/typescript_white.png' },
+  { name: 'JavaScript', src: 'assets/icons/white/javascript_white.png' },
+  { name: 'HTML', src: 'assets/icons/white/html_white.png' },
+  { name: 'CSS', src: 'assets/icons/white/css_white.png' },
+  { name: 'API', src: 'assets/icons/white/api_white.png' },
+  { name: 'Material', src: 'assets/icons/white/material_white.png' },
+  { name: 'Firebase', src: 'assets/icons/white/firebase_white.png' },
+  { name: 'OOP', src: 'assets/icons/white/oop_white.png' },
+  { name: 'Git', src: 'assets/icons/white/git_white.png' },
+  { name: 'Scrum', src: 'assets/icons/white/scrum_white.png' },
+  { name: 'React', src: 'assets/icons/colors/react_icon_color.png' },
+  { name: 'Vue', src: 'assets/icons/colors/vue_icon_color.png' },
+];
+
+
 }
