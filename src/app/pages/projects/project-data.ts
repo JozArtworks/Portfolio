eb4b5e6410c64b98d2b919b2d0e0398cd30b848c
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-07-08 19:49:07 +0200

    🎨♻️ style & refactor(projects): html structure change for better read

--- src/app/pages/projects/project-data.ts
+++ src/app/pages/projects/project-data.ts
@@ -1,12 +1,11 @@
 import { Project } from './project.interface';
 
-
 export const PROJECTS: Project[] = [
   {
     title: 'Join',
     description: 'Task-Manager mit Drag & Drop',
     dialogDescription: 'Task-Manager inspiriert vom Kanban-System. Erstelle und organisiere Aufgaben per Drag & Drop, weise Nutzer und Kategorien zu.',
-    image: 'assets/images/join-preview.png',
+    image: 'assets/images/web_image/join_image.png',
     icons: [
       'assets/icons/white/svg/logo_firebase.svg',
       'assets/icons/white/svg/logo_scrum.svg'
@@ -18,7 +17,7 @@ export const PROJECTS: Project[] = [
     title: 'Pokedex',
     description: 'Interaktive App mit API-Daten',
     dialogDescription: 'Interaktive Pokedex-App mit Live-API-Daten. Zeigt Namen, Typen und Bilder - filterbar und durchsuchbar.',
-    image: 'assets/images/join-preview.png',
+    image: 'assets/images/web_image/pokedex_image.png',
     icons: [
       'assets/icons/white/svg/logo_api.svg',
       'assets/icons/white/svg/logo_typescript.svg'
@@ -30,7 +29,7 @@ export const PROJECTS: Project[] = [
     title: 'El Pollo Loco',
     description: 'Gaming mit OOP',
     dialogDescription: 'Jump-, Run- und Wurfspiel mit objektorientierter Programmiertem Ansatz. Hilf Pepe, Münzen und Tabascosoße zu finden, um gegen das verrückte Huhn zu gewinnen.',
-    image: 'assets/images/el-pollo-loco-preview.png',
+    image: 'assets/images/web_image/elpolloloco_image.png',
     icons: [
       'assets/icons/white/svg/logo_keyboard.svg',
       'assets/icons/white/svg/logo_oop.svg'
@@ -42,7 +41,7 @@ export const PROJECTS: Project[] = [
     title: 'Portfolio',
     description: 'UI-Design & Animation',
     dialogDescription: 'Persönliches Portfolio mit Fokus auf UI-Design und sanften Animationen. Struktur, Stil und Bewegung sorgen für ein klares Nutzererlebnis.',
-    image: 'assets/images/portfolio-preview.png',
+    image: 'assets/images/web_image/portfolio_image.png',
     icons: [
       'assets/icons/white/svg/logo_angular.svg'
     ],
