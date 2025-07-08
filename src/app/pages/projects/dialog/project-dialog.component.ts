bd159e622fe7d9e293ed0687d12b0687edf513b0
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-07-08 13:27:07 +0200

    ✨feat(projects): added conditional navigation for project dialog

--- src/app/pages/projects/dialog/project-dialog.component.ts
+++ src/app/pages/projects/dialog/project-dialog.component.ts
@@ -12,9 +12,38 @@ import { Project } from '../project.interface';
 export class ProjectDialogComponent {
   @Input() project!: Project;
   @Input() show = false;
+  @Input() allProjects: Project[] = [];
+
   @Output() close = new EventEmitter<void>();
+  @Output() changeProject = new EventEmitter<Project>();
 
   onClose() {
     this.close.emit();
   }
+
+nextProject() {
+  if (!this.isLast) {
+    const next = this.allProjects[this.currentIndex + 1];
+    this.changeProject.emit(next);
+  }
+}
+
+previousProject() {
+  if (!this.isFirst) {
+    const prev = this.allProjects[this.currentIndex - 1];
+    this.changeProject.emit(prev);
+  }
+}
+
+  get currentIndex(): number {
+  return this.allProjects.findIndex(p => p.title === this.project.title);
+}
+
+get isFirst(): boolean {
+  return this.currentIndex === 0;
+}
+
+get isLast(): boolean {
+  return this.currentIndex === this.allProjects.length - 1;
+}
 }
