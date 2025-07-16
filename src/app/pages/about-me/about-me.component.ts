f98b1b80363c99c43a1e2f58472832779b34398d
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-07-16 12:43:47 +0200

    ✨🎨 feat & style(imprint&privacy): added ngx-translate support and improved back button with hover effect & SVG icon

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService, TranslateModule } from '@ngx-translate/core'; // beide hier zusammen
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 
 
--- src/app/pages/legal/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/legal/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { signal } from '@angular/core';
+import { LangSwitchComponent } from '../../../shared/components/lang-switch/lang-switch.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule, LangSwitchComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
+
+  constructor(private translate: TranslateService) { }
+
+  language = signal<'de' | 'en'>('de');
+
+  setLanguage(lang: 'de' | 'en') {
+    this.language.set(lang);
+    this.translate.use(lang);
+  }
+
+  iconLeft = 'assets/icons/white/svg/icon_left_white.svg';
+
+
+  onHoverLeft(hovered: boolean) {
+    this.iconLeft = hovered
+      ? 'assets/icons/green/svg/icon_left_green.svg'
+      : 'assets/icons/white/svg/icon_left_white.svg';
+  }
 
 }
