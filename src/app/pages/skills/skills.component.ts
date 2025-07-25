badb374b513dff3c71053f6b042d77f7782a84bd
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-07-25 14:22:27 +0200

    ✨♻️🎨 feat, refactor & style (skills): improve hover effects, questionmark, tooltip & responsive limit

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import {
   toolsIcons,
   extraTools,
   toolsIconsDesign,
+  ToolIcon
 } from './../../shared/data/tools-icons.data';
+
 @Component({
   selector: 'app-skills',
   standalone: true,
@@ -14,28 +16,75 @@ import {
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
-  showExtraTools = false;
-
   toolIcons = toolsIcons;
   extraTools = extraTools;
   toolIconsDesign = toolsIconsDesign;
+  showExtraTools = false;
 
-  onIconClick(icon: any) {
+  hoveredTooltipText = '';
+  tooltipX = 0;
+  tooltipY = 0;
+
+  constructor(private translate: TranslateService) { }
+
+  isMobileView = false;
+
+  ngOnInit(): void {
+    this.isMobileView = window.innerWidth > 1000;
+    window.addEventListener('resize', () => {
+      this.isMobileView = window.innerWidth > 1000;
+    });
+  }
+
+  onIconClick(icon: ToolIcon): void {
     if (icon.name === 'school') {
       this.toggleExtraTools();
     }
   }
 
-  toggleExtraTools() {
+  toggleExtraTools(): void {
     this.showExtraTools = !this.showExtraTools;
   }
 
-  onKeyDown(event: KeyboardEvent, icon: any) {
+  onKeyDown(event: KeyboardEvent, icon: ToolIcon): void {
     if (event.key === 'Enter' || event.key === ' ') {
       this.onIconClick(icon);
       event.preventDefault();
     }
+  }
+
+
+
+
+
+  cursorX = 0;
+  cursorY = 0;
+  showQuestionCursor = false;
+
+  onTooltipEnter(event: MouseEvent, key: string, iconName: string): void {
+    this.hoveredTooltipText = this.translate.instant(key);
+
+
+    this.showQuestionCursor = iconName === 'school';
+
+
+
+    this.cursorX = event.clientX;
+    this.cursorY = event.clientY;
+  }
+
+
+
+  onMouseMove(event: MouseEvent): void {
+    if (this.showQuestionCursor) {
+      this.cursorX = event.clientX + 20;
+      this.cursorY = event.clientY - 20;
+    }
+  }
+
+  onMouseLeaveTooltip(): void {
+    this.hoveredTooltipText = '';
+    this.showQuestionCursor = false;
   }
 
 }
