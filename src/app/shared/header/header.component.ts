37f1de224b5b5f729733cc4eefa27944afc9293c
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-07-26 16:32:28 +0200

    ✨♿ feat(header): translated logo alt text with ngx-translate for better accessibility

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,16 +2,17 @@ import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent, RouterModule],
+  imports: [NavbarComponent, RouterModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,) { }
 
   @Input() currentSection = 'home';
   @Input() isMobileView = false;
@@ -32,7 +33,7 @@ export class HeaderComponent {
     this.forceCloseMenu.emit();
   }
 
-    scrollToHome(event: Event) {
+  scrollToHome(event: Event) {
     event.preventDefault();
     this.router.navigate(['/']).then(() => {
       setTimeout(() => {
@@ -43,6 +44,5 @@ export class HeaderComponent {
       }, 100);
     });
   }
-
 
 }
